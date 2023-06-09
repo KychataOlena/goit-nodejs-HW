89607477f5a7d9af8d36ac6bf3ddb50abec4030f
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2023-06-09 23:18:35 +0300

    add HW -2/3/4

---
+++ routes/api/contacts.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const ctrl = require("../../controlers-decorator/contact-controler");
+
+const { schema } = require("../../models/contact");
+
+const { isValidId, authenticate } = require("../../middlewares");
+
+const validateBody = require("../../utils/validateBody");
+
+const router = express.Router();
+
+router.get("/", authenticate, ctrl.listContact);
+
+router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
+
+router.post(
+  "/",
+  authenticate,
+  validateBody(schema.contactAddSchema),
+  ctrl.addContact
+);
+
+router.put(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  validateBody(schema.contactAddSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schema.updateFavoriteSchema),
+  ctrl.updateFavoriteById
+);
+
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteContact);
+
+module.exports = router;
